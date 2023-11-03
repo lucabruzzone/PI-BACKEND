95399a8fb276a44e5c8e4f5feb190233cb049b1d
LUCAS <lucas@MacBook-Pro-de-LUCAS.local>
2023-11-03 05:34:56 -0300

    update

--- src/db.js
+++ src/db.js
@@ -6,6 +6,7 @@ const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+  dialectModule: require('pg'),
   logging: false, 
   native: false, 
 });
